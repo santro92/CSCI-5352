d904713b83e620218279897539769edd354fc7de
santro92 <jayramsan@gmail.com>
2016-10-15 13:07:41 -0600

    added other centralities

--- code/eigen-centrality.py
+++ code/eigen-centrality.py
@@ -5,12 +5,12 @@ for i in xrange( 10426, 0, -1 ):
     adjlist = str(i)+"-subgraph.adjlist"
     G = nx.read_adjlist("../subgraphs/"+adjlist)
 
-    print "Computing degree centrality"
-    dc = nx.degree_centrality( G )
+    print "Computing eigen centrality"
+    ec = nx.eigenvector_centrality( G )
 
     print "Writing to file"
     # write dictionary to file
     import json
-    with open('../centrality/'+adjlist+'-dc.json', 'w') as fp:
-        json.dump( dc, fp, sort_keys=True, indent=4)
+    with open('../centrality/'+adjlist+'-ec.json', 'w') as fp:
+        json.dump( ec, fp, sort_keys=True, indent=4)
 
