0e95a90ee74d6ba67d79184752b3f77bf1cfb6f8
ruhisaraf <ruhi.saraf14@gmail.com>
2016-10-15 12:23:37 -0600

    Update clean-data.py

--- code/clean-data.py
+++ code/clean-data.py
@@ -9,9 +9,9 @@ cleanDataDict = { 'Intermediaries': 3, 'Entities': 18,
                     'Officers': 2, 'Addresses': 2, 'all_edges': 4 }
 
 for key in cleanDataDict:
-    with open("data/"+str(key)+".csv","rb") as source:
+    with open("../data/"+str(key)+".csv","rb") as source:
         rdr= csv.reader( source )
-        with open("fullgraphs/"+str(key)+".csv","wb") as result:
+        with open("../fullgraphs/"+str(key)+".csv","wb") as result:
             wtr= csv.writer( result )
             for r in rdr:
                 del r[cleanDataDict[key]]
